7a0ec63e209bc4cb878f5b13937c38fb60d1c62a
ryanbae94 <ryanbae94@gmail.com>
2024-06-22 15:08:30 +0900

    feat: Parallax Banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from 'styled-components';
+import './App.css';
+import Header from './components/Header/Header';
+
+const ParallaxBanner = styled.div`
+	position: relative;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+	height: calc(100vh - 104px);
+	background-image: url('src/assets/main_bg.jpeg');
+	background-attachment: fixed;
+	background-size: cover;
+	background-repeat: no-repeat;
+	background-position: center;
+	margin-top: 104px;
+
+	&::before {
+		content: '';
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(0, 0, 0, 0.4);
+	}
+`;
+
+const BannerWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	gap: 20px;
+	z-index: 10;
+`;
+
+const Badge = styled.div`
+	width: 151px;
+	height: 26px;
+	font-size: 12px;
+	font-weight: 500;
+	letter-spacing: 0.1em;
+	color: var(--color-white);
+	border: 1px solid var(--color-white);
+	border-radius: 20px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+const Title = styled.h1`
+	font-weight: 700;
+	font-size: 60px;
+	line-height: 65px;
+	color: var(--color-white);
+`;
+
+const DescriptionWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+const Description = styled.p`
+	font-weight: 500;
+	font-size: 16px;
+	line-height: 20px;
+	color: var(--color-white);
+`;
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: 100%;
+	max-width: 1728px;
+	height: 4000px;
+	margin: 0 auto;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<Container>
+			<Header />
+			<ParallaxBanner>
+				<BannerWrapper>
+					<Badge>INDOOR PANEL</Badge>
+					<Title>M4</Title>
+					<DescriptionWrapper>
+						<Description>Pixel pitch │ p0.7 p0.9 p1.2 p1.5 p1.8</Description>
+						<Description>
+							Application │ Commercial Space Advertising Lobby Church
+						</Description>
+					</DescriptionWrapper>
+				</BannerWrapper>
+			</ParallaxBanner>
+		</Container>
+	);
 }
 
-export default App
+export default App;
